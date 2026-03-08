fe78cba2c6a233a54927957cd3af9b3f45f147bd
soloking1412 <maheswar141203@gmail.com>
2026-03-08 23:41:43 +0530

    Version fix

--- src/cli.ts
+++ src/cli.ts
@@ -16,7 +16,7 @@ const program = new Command();
 program
   .name('stylus-toolkit')
   .description('A comprehensive CLI development environment for Arbitrum Stylus smart contracts')
-  .version('0.2.11');
+  .version('1.0.0');
 
 program
   .command('init')
